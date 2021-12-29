dab0ca9ced4da053278712bbc29bb392429e0239
marenjoseph <95083930+marenjoseph@users.noreply.github.com>
2021-12-29 12:04:40 +0100

    Update Activity.js

--- components/Activity.js
+++ components/Activity.js
@@ -6,7 +6,6 @@ import { getData } from "../utils/storyblok"
 import InPageSlideshow from "./InPageSlideshow"
 import RelatedItemGallery from "./RelatedItemGallery"
 
-//test
 
 const Activity = ({ data, level }) => {
   var locale = 'en';
