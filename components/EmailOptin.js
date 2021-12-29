5f72716be6a414fbdf6571aa69faee6a13bb0f30
marenjoseph <maren.joseph@ugent.be>
2021-12-29 18:18:07 +0100

    SendInBleu

--- components/EmailOptin.js
+++ components/EmailOptin.js
@@ -10,7 +10,9 @@ const EmailOptin = ({ }) => {
     
     <div className={styles.emailoptin}>
       <div className={styles.optinbutton} onClick={() => toggleformstate()}>Keep me posted through the newsletter</div>
-      {/* {formstate&&<iframe width="540" height="900" src="" frameborder="0" scrolling="auto" allowfullscreen style={{ display: `block`, marginLeft: `auto`, marginRight: `auto`, maxWidth: '100%' }}></iframe>} */}
+       {formstate&&<iframe width="540" height="900" src="charlotte.beeuwsaert@ugent.be" frameborder="0" scrolling="auto" allowfullscreen style={{ display: `block`, marginLeft: `auto`, marginRight: `auto`, maxWidth: '100%' }}></iframe>}
+
+
     </div>
   );
 };
