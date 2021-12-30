b229ea2a9eabdb1271f393c414afd7f00a54d3b5
marenjoseph <maren.joseph@ugent.be>
2021-12-30 23:15:47 +0100

    hotjar + leadfeeder

--- pages/_document.js
+++ pages/_document.js
@@ -22,7 +22,7 @@ export default class MyDocument extends Document {
             }}
           />
           <script dangerouslySetInnerHTML={{
-            __html: ` (function(ss,ex){ window.ldfdr=window.ldfdr||function(){(ldfdr._q=ldfdr._q||[]).push([].slice.call(arguments));}; (function(d,s){ fs=d.getElementsByTagName(s)[0]; function ce(src){ var cs=d.createElement(s); cs.src=src; cs.async=1; fs.parentNode.insertBefore(cs,fs); }; ce('https://sc.lfeeder.com/lftracker_v1_'+ss+(ex?'_'+ex:'')+'.js'); })(document,'script'); })('ywVkO4XWVAp7Z6Bj');('${process.env.NEXT_PUBLIC_LEADFEEDER}');`
+            __html: ` (function(ss,ex){ window.ldfdr=window.ldfdr||function(){(ldfdr._q=ldfdr._q||[]).push([].slice.call(arguments));}; (function(d,s){ fs=d.getElementsByTagName(s)[0]; function ce(src){ var cs=d.createElement(s); cs.src=src; cs.async=1; fs.parentNode.insertBefore(cs,fs); }; ce('https://sc.lfeeder.com/lftracker_v1_'+ss+(ex?'_'+ex:'')+'.js'); })(document,'script'); })('${process.env.NEXT_PUBLIC_LEADFEEDER}');`
           }}
           />
 
